bd3bd72db009194d09271bca08c11f27cf16903f
hindu <hindumahiavuluri@gmail.com>
2024-12-27 04:34:43 +0000

    code

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,4 +1,6 @@
 // Write your code here
+import Popup from 'reactjs-popup'
+import Payment from '../Payment'
 import CartContext from '../../context/CartContext'
 
 import './index.css'
@@ -10,7 +12,7 @@ const CartSummary = () => (
 
       const itemsPrice = []
 
-      const cartItemsPrices = cartList.map(eachItem =>
+      cartList.map(eachItem =>
         itemsPrice.push(eachItem.price * eachItem.quantity),
       )
 
@@ -24,9 +26,22 @@ const CartSummary = () => (
             Order Total: <span className="price"> Rs {totalPrice}</span>
           </h2>
           <p className="cart-length">{cartList.length} items in cart</p>
-          <button className="button" type="button">
-            Checkout
-          </button>
+
+          {
+            // <button className="button" type="button">  Checkout </button> }
+
+            <Popup
+              modal
+              trigger={
+                <button className="checkout-btn" type="button">
+                  Checkout
+                </button>
+              }
+              position="top left"
+            >
+              {close => <Payment close={close} />}
+            </Popup>
+          }
         </div>
       )
     }}
